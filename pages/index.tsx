43a8b7b31082a7ef3f09e38feef8202e0b3e1075
troob <troobengineer@gmail.com>
2024-07-08 21:56:03 -0400

    added per user data

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function App() {
     <Authenticator>
       {({signOut, user}) => (
         <main>
-          <h1>{user?.signInDetails?.loginId}'s todos</h1>
+          <h1>{user?.signInDetails?.loginId} todos</h1>
           <button onClick={createTodo}>+ new</button>
           <ul>
             {todos.map((todo) => (
